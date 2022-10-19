7dff9717418512c199b4cc2f564bd3798b969bb5
Jpschnabel177 <jpschnabel96@gmail.com>
2022-10-19 09:32:06 -0400

    finished It1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
+console.log("I'm ready!");
 // Iteration 1: Names and Input
-//
+let hacker1 = "jp";
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "unknown";
+console.log(`The navigator's name is ${hacker2}`);
 // Iteration 2: Conditionals
 
-
 // Iteration 3: Loops
+
